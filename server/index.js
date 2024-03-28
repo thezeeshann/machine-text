925f0db9a28fde4f80589518249a2f1517310b78
thezeeshann <info.zeeshann@gmail.com>
2024-03-28 13:22:53 +0530

    init

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-import express from "express"
+import express from "express";
+import categoryRoute from "./routes/categoryRoute.js";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+const PORT = 8000;
 
-const PORT = 8000
+app.use("/api/v1/category", categoryRoute);
 
 app.get("/", (req, res) => {
-    res.send("Your server is running up")
-})
-
+  res.send("Your server is running up");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`)
-})
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
