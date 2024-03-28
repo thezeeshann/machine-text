c064bcaadb0da1d9411f496397570726aae6e54a
thezeeshann <info.zeeshann@gmail.com>
2024-03-28 14:34:39 +0530

    feat: added category & product routes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import categoryRoute from "./routes/categoryRoute.js";
+import productRoutes from "./routes/productRoute.js";
 
 const app = express();
 app.use(express.json());
 const PORT = 8000;
 
 app.use("/api/v1/category", categoryRoute);
+app.use("/api/v1/product", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("Your server is running up");
