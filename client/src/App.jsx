90e55de5c276cc0e651352e22cc4c2d92de5181b
thezeeshann <info.zeeshann@gmail.com>
2024-03-29 16:15:41 +0530

    feat: added node package

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
-
+import { Routes, Route } from "react-router-dom";
+import Products from "./pages/Products";
+import AddProduct from "./pages/AddProduct";
+import UpdateProduct from "./pages/UpdateProduct";
 
 function App() {
-
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+    <main className="w-full min-h-screen px-10 py-10 overflow-x-hidden bg-gray-300">
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/addProduct" element={<AddProduct />} />
+        <Route path="/updateProduct/:id" element={<UpdateProduct />} />
+      </Routes>
+    </main>
+  );
 }
 
-export default App
+export default App;
